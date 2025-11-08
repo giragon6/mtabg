0c03efe671fec55872c99e07e393d99d3f6305a3
giragon6 <emmemcdonald66@gmail.com>
2025-11-08 18:51:58 -0500

    feat: add fetching by query to cardFetcher

--- util/cardFetcher.ts
+++ util/cardFetcher.ts
@@ -5,7 +5,7 @@ class CardFetcher {
     "User-Agent"  : "MTabG/1.0"
   })
 
-  async fetch(identifiers: string[]): Promise<JSON[]> {
+  async fetchCardsById(identifiers: string[]): Promise<JSON[]> {
     let cardsJson: JSON[]
     const body = {
       identifiers: identifiers.map(id => ({id}))
@@ -28,5 +28,34 @@ class CardFetcher {
       console.error('Error fetching data: ', error)
     })
     return cardsJson
+  }
+
+  async fetchRandomCardsByQuery(query: string, num: number) {
+    let currentUrl = this.url + "search?q=" + query;
+    let hasMore = true;
+    let cardsJson: JSON[] = []
+    while (hasMore) {
+      fetch(currentUrl, { 
+          headers: this.headers,
+          method: "GET",
+      })
+      .then(response => {
+        if (!response.ok) throw new Error('Card response wasn\'t ok');
+        return response.json();
+      }) 
+      .then(data => {
+        if (!data.data || !data.has_more) throw new Error('Response had unexpected structure');
+        hasMore = data.has_more;
+        cardsJson.push.apply(cardsJson, data.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+      })
+    }
+    let chosenCards: JSON[];
+    for (let i = 0; i <= num; i++) {
+      chosenCards.push(cardsJson[Math.floor(Math.random() * cardsJson.length)]);
+    }
+    return chosenCards;
   }
 }
