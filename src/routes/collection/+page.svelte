2ad8c32f81b13db4495d509235a33e098769e563
giragon6 <emmemcdonald66@gmail.com>
2026-02-14 22:53:58 -0500

    add indexeddb usage bar

--- src/routes/collection/+page.svelte
+++ src/routes/collection/+page.svelte
@@ -4,14 +4,20 @@
 
 <script lang="ts">
 	import CardContainer from '$lib/components/card/CardContainer.svelte';
+  import QuotaStatus from './components/QuotaStatus.svelte'
   import { MTabGStorage } from '$lib/storage/storage'
   
   import { onMount } from 'svelte';
 
   let cards: Card[] = $state([]);
+  let storageUsedProgress: number | null = $state(null);
 
   onMount(async () => {
     cards = await MTabGStorage.getAllCards();
+    const quotaStatus: QuotaReport = await MTabGStorage.getEstimatedQuota();
+    const storageQuota = quotaStatus.quota;
+    const storageUsed = quotaStatus.usage;
+    storageUsedProgress = (storageUsed && storageQuota) ? storageUsed / storageQuota : null;
   })
 
   async function clearCards() {
@@ -26,5 +32,13 @@
 </script>
 
 <a href="/newtab"><button>Go back</button></a>
-<button on:click={clearCards}>Clear collection</button>
+<button onclick={clearCards}>Clear collection</button><br>
+<label for="storageUsed">Browser storage used:</label>
+<QuotaStatus id="storageUsed" progress={storageUsedProgress} />
 <CardContainer cards={cards} />
+
+<style>
+  label {
+    color: white;
+  }
+</style>
---
+++ src/routes/collection/components/QuotaStatus.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+  export let progress: number; 
+</script>
+
+<div class="border">
+  {progress ? progress.toFixed(2)+'%' : 'Error displaying progress'}
+  <div class="inner" style="width:{progress ? Math.min(progress, 100).toFixed(2)+'%' : '0%'}"></div>
+</div>
+
+<style>
+  .border {
+    width: 75%;
+    border: black 4px solid;
+    background: white;
+    text-align: center;
+  }
+
+  .inner {
+    background: #c93b28;
+  }
+</style>
